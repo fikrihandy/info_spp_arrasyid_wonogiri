daf91b39b4805475e6174a9ea9ada2219c0fb81d
Abdullah Fikri Handi Saputra <abdullahfikrihandi@gmail.com>
2022-10-19 18:17:02 +0700

    Update..

--- google_spreadsheets/__init__.py
+++ google_spreadsheets/__init__.py
@@ -5,6 +5,7 @@ g_docs = 'https://docs.google.com/spreadsheets/'
 s_id = f'{g_docs}d/e/2PACX-1vQS26yicc3kSA3jHy3UInX-oUDGfPj2TNs5Zxffg2MPDOEisbf3naU7EvR9-VJZoQ6iRrqwPP0zKXe3/pub?gid='
 
 # 3. ID sheet tiap Angkatan
+data_21 = '1927772519'
 data_22 = '0'
 data_23 = '644520043'
 
